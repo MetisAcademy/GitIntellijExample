cd7d3f27e48ba3c7916ba739a40d05c46591f1f7
MetisAcademy <java.lektor@gmail.com>
2023-05-27 15:45:22 +0200

    Added greeting in Slovak

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
         //Toto je koment
         System.out.println("Hello world!");
+
+        System.out.println("Ahojte vsetcia!");
     }
 
 }
